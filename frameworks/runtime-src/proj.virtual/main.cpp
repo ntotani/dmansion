b3a09d51341e2430fc7fa8df50402d038f71c8f7
Naoyuki Totani <nyk.totani@gmail.com>
2015-06-23 01:50:27 +0900

    Virtualize Director

--- frameworks/runtime-src/proj.virtual/main.cpp
+++ frameworks/runtime-src/proj.virtual/main.cpp
@@ -9,12 +9,23 @@
 
 USING_NS_CC;
 
+VirtualDirector* vd = nullptr;
+
+int set_delta_time_glue(lua_State* L)
+{
+    vd->setDeltaTime(lua_tonumber(L, 1));
+    return 0;
+}
+
 class AppDelegate : private Application {
     virtual bool applicationDidFinishLaunching() {
+        vd = VirtualDirector::create();
         auto engine = LuaEngine::getInstance();
         ScriptEngineManager::getInstance()->setScriptEngine(engine);
         lua_State* L = engine->getLuaStack()->getLuaState();
         lua_module_register(L);
+        lua_getglobal(L, "_G");
+        lua_register(L, "setDeltaTime", set_delta_time_glue);
         if (engine->executeScriptFile("test/main.lua"))
         {
             return false;
@@ -29,6 +40,7 @@ int main(int argc, char **argv)
 {
     // create the application instance
     AppDelegate app;
+    FileUtils::getInstance()->setDefaultResourceRootPath(argv[1]);
     FileUtils::getInstance()->addSearchPath(argv[1]);
     return Application::getInstance()->run();
 }
--- test/main.lua
+++ test/main.lua
@@ -3,12 +3,23 @@ cc.FileUtils:getInstance():setPopupNotify(false)
 cc.FileUtils:getInstance():addSearchPath("src/")
 cc.FileUtils:getInstance():addSearchPath("res/")
 
---require "config"
---require "cocos.init"
+require "config"
+require "cocos.init"
 
 local function main()
     --require("app.MyApp"):create():run()
-    print("hello test/main.lua")
+    local scene = cc.Scene:create()
+    cc.Director:getInstance():runWithScene(scene)
+    cc.Director:getInstance():mainLoop() -- run scene
+
+    local node = cc.Node:create()
+    scene:addChild(node)
+    node:runAction(cc.MoveTo:create(5, cc.p(10, 0)))
+    cc.Director:getInstance():mainLoop() -- first tick
+
+    setDeltaTime(5)
+    cc.Director:getInstance():mainLoop() -- animation
+    assert(node:getPositionX() == 10, "node should move right")
 end
 
 local status, msg = xpcall(main, __G__TRACKBACK__)
